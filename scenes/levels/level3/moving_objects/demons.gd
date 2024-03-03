08c3ccf6279af00b877cc79731f80b3d7e9ab266
carsin <20868514+carsin@users.noreply.github.com>
2024-03-02 20:21:35 -0800

    bat death working

--- scenes/levels/level3/moving_objects/demons.gd
+++ scenes/levels/level3/moving_objects/demons.gd
@@ -1,29 +1,17 @@
-extends CharacterBody2D
+extends Area2D
 
-
-const SPEED = 300.0
-var motion = Vector2()
+const SPEED = 50
+var velocity = Vector2()
 @onready var bat = $AnimatedSprite2D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		
 	bat.flip_h = true
 	
 	var DanteLevel3 = get_parent().get_node("DanteLevel3")
-	position += (DanteLevel3.position - position)/100
+	position += ((DanteLevel3.position - position)/100) * SPEED * delta
 	look_at(DanteLevel3.position)
-	
 
-	move_and_collide(motion)
-
-# not working
-func _on_area_2d_body_entered(body):
-	if "PieBullets" in body.name:
-		queue_free()
+func _on_area_entered(area):
+	print(area)
+	if "BulletArea" in area.name:
+		queue_free()	
