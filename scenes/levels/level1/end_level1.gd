f227180062f0bfe7223657e0987399cc590fd93b
kenzieke <kettwigmackenzie@gmail.com>
2024-03-06 16:16:21 -0500

    added Acheron's sprite

--- scenes/levels/level1/end_level1.gd
+++ scenes/levels/level1/end_level1.gd
@@ -3,7 +3,7 @@ extends Control
 
 @onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
 @export var rule_pressed_screen = preload("res://scenes/levels/level1/rule_pressed_level1.tscn") as PackedScene
-@export var welcome_screen = preload("res://scenes/levels/level1/welcome_screen.tscn") as PackedScene
+@export var acheron_meeting = preload("res://scenes/levels/level2/dialogue_scenes/dialogue1.tscn") as PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,4 +15,4 @@ func on_start_pressed() -> void:
 		get_tree().change_scene_to_packed(rule_pressed_screen)
 	# this works
 	else:
-		get_tree().change_scene_to_packed(welcome_screen)
+		get_tree().change_scene_to_packed(acheron_meeting)
