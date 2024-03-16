26f00b5f6ae5ed9de925293f06afcd96d3a40a4d
kenzieke <kettwigmackenzie@gmail.com>
2024-03-16 11:24:28 -0400

    Making level 6, made character and level base

---
+++ scenes/levels/level6/characters/flying_dante.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+@onready var dante_flying = $AnimatedSprite2D
+
+const GRAVITY : int = 1000
+const MAX_VELOCITY : int = 600
+const JET_SPEED : int = -500
+var flying : bool = false
+var falling : bool = false
+const START_POS = Vector2(100, 400)
+
+func _ready():
+	reset()
+	
+func reset():
+	falling = false
+	flying = false
+	position = START_POS
+	set_rotation(0)
+
+func _physics_process(delta):
+	if flying or falling:
+		velocity.y += GRAVITY * delta
+		if velocity.y > MAX_VELOCITY:
+			velocity.y = MAX_VELOCITY
+		if flying:
+			set_rotation(deg_to_rad(velocity.y * 0.05))
+			dante_flying.play()
+		elif falling:
+			set_rotation(PI/2)
+			dante_flying.stop()
+		move_and_collide(velocity * delta)
+	else:
+		dante_flying.stop()
+		
+func flap():
+	velocity.y = JET_SPEED
---
+++ scenes/levels/level6/level6.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+@onready var flying_dante = $FlyingDante
+
+var game_running : bool
+var game_over : bool
+var scroll
+var score
+const SCROLL_SPEED : int = 4
+var screen_size : Vector2i
+var ground_height : int
+var pipes : Array
+const FIRE_DELAY : int = 100
+const FIRE_RANGE : int = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	new_game()
+
+func new_game():
+	game_running = false
+	game_over = false
+	score = 0
+	scroll = 0
+	flying_dante.reset()
+	
+func _input(event):
+	if game_over == false:
+		if event is InputEventMouseButton:
+			if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+				if game_running == false:
+					start_game()
+				else:
+					if flying_dante.flying:
+						flying_dante.flap()
+
+func start_game():
+	game_running = true
+	flying_dante.flying = true
+	flying_dante.flap()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
