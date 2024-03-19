b01d6a9b639c0889cf2f1621dbfcc4e9e07b5624
carsin <20868514+carsin@users.noreply.github.com>
2024-03-18 17:42:17 -0700

    lvl5 win/lose conditions

--- scenes/levels/level5/level5.gd
+++ scenes/levels/level5/level5.gd
@@ -6,20 +6,19 @@ extends Node2D
 @onready var player = $Player
 @onready var window_size = get_viewport_rect().size
 
-const INIT_SPEED = 50
-var target_speed = INIT_SPEED
-var accel = 700
+var init_speed = 15
+var target_speed = init_speed
+var accel = 600
 var current_lane = 0
 var target_lane = 0
 var lane_positions = []
 var player_may_move = true
 var points = 0
+var lost_points = 0
+const POINTS_TO_WIN = 20
+const L_POINTS_TO_LOSE = 6
 @onready var target_catch_offset = $Player/CollisionShape2D.shape.extents.y
 
-func player_moved():
-	player_may_move = false
-	move_timer.start()
-	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	faller_timer.start()
@@ -39,31 +38,56 @@ func _input(event):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	init_speed += 5 * delta
+	accel += 8 * delta
 	player.position.x = lane_positions[current_lane]
-	
 	target_speed += accel * delta
 	faller.position.y += target_speed * delta
 	faller.position.x = lane_positions[target_lane]
 	
+	# check if object caught
 	if current_lane == target_lane:
 		if faller.position.y < player.position.y:
 			faller.position.y = clamp(faller.position.y, 0, player.position.y + target_catch_offset)
 			
 		if faller.position.y >= player.position.y and faller.position.y < player.position.y + target_catch_offset:
 			target_fell(true)
+			
+	# check if won
+	if points >= POINTS_TO_WIN:
+		print("GAMEOVER! You win!")
+		faller_timer.stop()
+		# TODO: game over, progress to next level
+		
+	if lost_points >= L_POINTS_TO_LOSE:
+		restart_level()
 
 func _on_falling_timer_timeout():
 	target_fell(false)
 
+func restart_level():
+	print("YOU LOST!")
+	points = 0
+	lost_points = 0
+	player_may_move = true
+	get_tree().reload_current_scene()
+
 func target_fell(caught):
 	if caught and current_lane == target_lane:
 		points += 1
-		
+	elif not caught:
+		lost_points += 1
+	
+	
 	print(points)
-	faller.position.y = 0
-	target_speed = INIT_SPEED
+	faller.position.y = 0 - 4 * $FallingObject/CollisionShape2D.shape.extents.y
+	target_speed = init_speed
 	target_lane = randi_range(0, 3)
 	faller_timer.start()
+	
+func player_moved():
+	player_may_move = false
+	move_timer.start()
 
 func _on_move_timer_timeout():
 	player_may_move = true
